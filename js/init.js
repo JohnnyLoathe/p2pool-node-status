d8bec3b002cd39dd8c6c0a21b1c8fa15fc3e78d6
Justin Ramos <justin.ramos@gmail.com>
2014-07-27 18:43:35 +0000

    reordering loads

--- js/init.js
+++ js/init.js
@@ -4,14 +4,6 @@ var api_url= "";
 $(document).ready(function() {
   $(document).trigger('init');
 
-  // load header and footer
-  if(config.header_url && config.header_url.length > 0) {
-    $("#header").load(config.header_url);
-  }
-  if(config.footer_url && config.footer_url.length > 0) {
-    $("#footer").load(config.footer_url);
-  }
-
   // set node name
   if(config.host && config.host.length > 0) {
     api_url= jsonp + '&host=' + encodeURI(config.host) + '&report=';
@@ -20,6 +12,14 @@ $(document).ready(function() {
   } else if (config.node_name) {
     $('#node').removeClass('hidden').text(config.node_name);
     $('#_node').removeClass('hidden');
+  }
+
+  // load header and footer
+  if(config.header_url && config.header_url.length > 0) {
+    $("#header").load(config.header_url);
+  }
+  if(config.footer_url && config.footer_url.length > 0) {
+    $("#footer").load(config.footer_url);
   }
 
   // set updated at
