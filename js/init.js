321c700f21d65e2f882e95c7969edcf688fdec43
Justin Ramos <justin.ramos@gmail.com>
2014-07-27 18:25:32 +0000

    refactoring common JS

---
+++ js/init.js
@@ -0,0 +1,31 @@
+var jsonp= "./jsonp.php?callback=?";
+var api_url= "";
+
+$(document).ready(function() {
+  $(document).trigger('init');
+
+  // load header and footer
+  if(config.header_url && config.header_url.length > 0) {
+    $("#header").load(config.header_url);
+  }
+  if(config.footer_url && config.footer_url.length > 0) {
+    $("#footer").load(config.footer_url);
+  }
+
+  // set node name
+  if(config.host && config.host.length > 0) {
+    api_url= jsonp + '&host=' + encodeURI(config.host) + '&report=';
+    $('#node').removeClass('hidden').text(config.host);
+    $('#_node').removeClass('hidden');
+  } else if (config.hostname) {
+    $('#node').removeClass('hidden').text(config.hostname);
+    $('#_node').removeClass('hidden');
+  }
+
+  // set updated at
+  dts= $.format.date(new Date(), 'yyyy-MM-dd HH:mm:ss');
+  $('#updated').text(dts);
+
+  // set donation address
+  $('.donation_address').text(config.donation_address);
+});
