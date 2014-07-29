2363497d0d261c35733caeeb5e314143089e0119
Justin Ramos <jramos@changehealthcare.com>
2014-07-29 16:52:08 +0000

    reordering

--- js/init.js
+++ js/init.js
@@ -3,24 +3,25 @@ var node_address_link = "";
 $(document).ready(function() {
   $(document).trigger('init');
 
-  // set node name
-  if (config.node_name) {
-    $('#node').removeClass('hidden').text(config.node_name);
-    $('#_node').removeClass('hidden');
-  }
-
-  if(config.donation_address && config.donation_address.length > 0) {
-    node_address_link = $('<a/>')
-      .attr('href', 'https://blockchain.info/address/' + config.donation_address)
-      .attr('target', '_blank').text(config.donation_address);
-  }
-
   // load header and footer
   if(config.header_url && config.header_url.length > 0) {
     $("#header").load(config.header_url);
   }
   if(config.footer_url && config.footer_url.length > 0) {
     $("#footer").load(config.footer_url);
+  }
+
+  // set node name
+  if (config.node_name) {
+    $('#node').removeClass('hidden').text(config.node_name);
+    $('#_node').removeClass('hidden');
+  }
+
+  // create donation address link
+  if(config.donation_address && config.donation_address.length > 0) {
+    node_address_link = $('<a/>')
+      .attr('href', 'https://blockchain.info/address/' + config.donation_address)
+      .attr('target', '_blank').text(config.donation_address);
   }
 
   // set updated at
