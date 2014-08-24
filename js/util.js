2669e0d709d315656ec7be7ced30f15276433081
Justin Ramos <jramos@changehealthcare.com>
2014-08-24 21:11:20 +0000

    better ad rotation

--- js/util.js
+++ js/util.js
@@ -108,7 +108,14 @@ var toggleAudio = function () {
 
 var rotateAd = function () {
   if(config.ad_url && config.ad_url.length > 0) {
-    $("#ad").load(config.ad_url);
+    $("#ad").load(config.ad_url, function() {
+      if ($('.ad').length > 1) {
+        to_show = Math.round(Math.random());
+        $('.ad')[to_show].style.display = 'block';
+      } else if ($('.ad').length == 1) {
+        $('.ad')[0].style.display = 'block';
+      }
+    });
   }
 }
 
